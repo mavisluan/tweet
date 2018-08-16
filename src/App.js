f5645cf9055d7935f52b16fb58030b20bc02b029
Yi Angelov <mavisluan83@gmail.com>
2018-08-15 17:12:19 -0700

    update App

--- src/App.js
+++ src/App.js
@@ -1,14 +1,29 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './App.css';
+import { tweets } from './static-data'
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
+const Tweet = () => (
+  <ul>
+    {tweets.map(tweet => (
+      <li key={tweet.id}>
+        <Card item={tweet}/>
+      </li>
+    ))}
+  </ul>
+)
 
-      </div>
-    );
-  }
-}
-
-export default App;
+const Card = ({ item: { id, userName, avatar, handleName, message, timeStamp, retweet, like} }) => (
+  <div className='card'>
+    <div className='card-avatar'>
+      <img src={avatar} height='50px' width='50px' alt={userName}/>
+    </div>
+    <div className='card-info'>
+      <span>{userName}</span>
+      <span>{handleName}</span>
+      <span>Time</span>
+      <p>{message}</p>
+      <div className='card-buttons'>buttons</div>
+    </div>
+  </div>
+)
+export default Tweet;
